9b87dcfe5cd8466260f2de2c986df1c99e7320c6
Viktor Marusiak <viktor.marusiak@student.tuke.sk>
2024-04-10 18:11:21 +0200

    add signup function and button

--- pokedex/pokedex/views.py
+++ pokedex/pokedex/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
-    links = [
-        { 'title': 'Home', 'url': '/'},
-        { 'title': 'Pokémons', 'url': '/pokedex'},
-        { 'title': 'Admin', 'url': '/admin', 'target': '_new'},
-    ]
+    links = {
+        'links': [
+            {'title': 'Home', 'url': '/'},
+            {'title': 'Pokémons', 'url': '/pokedex'},
+            {'title': 'Admin', 'url': '/admin', 'target': '_new'},
+            {'title': 'Sign Up', 'url': '/auth/signup', 'target': '_new'},
+        ]
+    }
 
     return render(request, "home.tpl.html", context=links)
