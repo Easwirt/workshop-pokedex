cb91d17bfc7d1bc6fbc3cae69bdf52a2153eca4a
Valerii Bondariev <valerii.bondariev@student.tuke.sk>
2024-04-10 18:09:46 +0200

    static upd

--- pokedex/pokedex/views.py
+++ pokedex/pokedex/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
-    links = [
+    links = {
+        'links': [
         { 'title': 'Home', 'url': '/'},
         { 'title': 'Pokémons', 'url': '/pokedex'},
         { 'title': 'Admin', 'url': '/admin', 'target': '_new'},
-    ]
-
+        ]
+    }
     return render(request, "home.tpl.html", context=links)
+
+
