0b11c476ad249d827df34ec2d5195fb1442b8ee3
Viktor <viktor.marusiak@student.tuke.sk>
2024-04-23 01:38:00 +0200

    change links method to template

--- pokedex/pokedex/views.py
+++ pokedex/pokedex/views.py
@@ -1,18 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    links = {
-        'links': [
-        { 'title': 'Sign in', 'url': '/auth/signin'},
-        { 'title': 'Sign Up', 'url': '/auth/signup'},
-        ]
-    }
-
-    if request.user.is_authenticated:
-        links['links'].pop()
-        links['links'].pop()
-        links['links'].append({ 'title': 'Pokémons', 'url': '/pokemons'})
-        links['links'].append({ 'title': 'Logout', 'url': '/auth/signout'})
-        links['links'].append({ 'title': 'Profile', 'url': '/id/'})
-
-    return render(request, "home/home.tpl.html", context=links)
+    return render(request, "home/home.tpl.html")
