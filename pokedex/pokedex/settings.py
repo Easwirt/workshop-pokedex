590965c2e80f1c36078cdee7599d9a2ba8d654c0
Valerii Bondariev <valerii.bondariev@student.tuke.sk>
2024-05-26 22:08:12 +0200

    finally deploy upd

--- pokedex/pokedex/settings.py
+++ pokedex/pokedex/settings.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_path = os.path.join(BASE_DIR, '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
