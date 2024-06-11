3ba1981c994515a7d2acdf62e88a29aa2013bbdc
Valerii Bondariev <valera.bondarev2005@gmail.com>
2024-06-11 16:34:25 +0200

    fix reset online status

--- pokedex/pokedex/asgi.py
+++ pokedex/pokedex/asgi.py
@@ -13,15 +13,28 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from . import routing
-
+from django.contrib.auth import get_user_model
+from asgiref.sync import sync_to_async
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokedex.settings')
 
 django_asgi_app = get_asgi_application()
 django.setup()
 
+User = get_user_model()
 
-application = ProtocolTypeRouter({
+@sync_to_async
+def update_user_status():
+    User.objects.all().update(online_status=0)
+
+class Protocol(ProtocolTypeRouter):
+    async def __call__(self, scope, receive, send):
+        await update_user_status()
+        return await super().__call__(scope, receive, send)
+
+
+
+application = Protocol({
     'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
--- pokedex/users/apps.py
+++ pokedex/users/apps.py
@@ -13,15 +13,3 @@ def warning_ignore(func):
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-
-    @warning_ignore
-    def ready(self):
-        from .models import User
-        post_save.connect(online_status_reset, sender=User)
-        User = get_user_model()
-        User.objects.all().update(online_status=0)
-
-def online_status_reset(sender, instance, **kwargs):
-    if instance.online_status != 0:
-        instance.online_status = 0
-        instance.save()
