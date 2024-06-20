007d1306cdec0df462f8d5482e87cdbb57d140bd
Viktor <viktor.marusiak@student.tuke.sk>
2024-06-21 00:45:31 +0200

    restore searchbar at home page, make another template for income messages, fix styles in income messages, add empty title when income messages is empty

--- pokedex/static/js/incomeMessage.js
+++ pokedex/static/js/incomeMessage.js
@@ -32,33 +32,44 @@ document.addEventListener("DOMContentLoaded", function (){
                 data.friends.forEach(friend => {
                     let listItem = document.createElement('li')
                     listItem.style.display = "block"
+                    listItem.style.margin = '10px'
 
                     let avatarImg = document.createElement('img')
                     avatarImg.src = getImgLink() + `${friend.avatar}` + '.png'
                     avatarImg.alt = `${friend.username}'s avatar`
                     avatarImg.style.width = '50px';
                     avatarImg.style.height = '50px';
+                    avatarImg.style.marginRight = '5px';
+                    avatarImg.style.borderRadius = '50%'
                     listItem.appendChild(avatarImg);
 
                     let usernameSpan = document.createElement('span')
                     usernameSpan.textContent = friend.username
+                    usernameSpan.style.marginRight = '5px';
                     listItem.appendChild(usernameSpan)
 
                     let acceptBtn = document.createElement('button')
                     acceptBtn.textContent = 'Accept';
-                    acceptBtn.className = 'addFriendBtn';
+                    acceptBtn.className = 'addFriendBtn btn btn-success';
+                    acceptBtn.style.marginRight = '5px'
                     listItem.appendChild(acceptBtn);
                     nameList.push(friend.username);
                     addFriendBtns.push(acceptBtn);
 
                     let declineBtn = document.createElement('button')
                     declineBtn.textContent = 'Decline';
+                    declineBtn.className = 'btn btn-danger';
                     listItem.appendChild(declineBtn);
                     declineFriendBtns.push(declineBtn);
 
                     friendsList.appendChild(listItem);
                     hide.push(listItem);
                 })
+                if(data.friends.length === 0){
+                    let empty = document.createElement('h3')
+                    empty.textContent = 'Empty'
+                    friendsList.appendChild(empty)
+                }
                 click(nameList, hide);
                 decline(nameList, hide);
             })
@@ -173,3 +184,6 @@ document.addEventListener("DOMContentLoaded", function (){
         return cookieValue;
     }
 })
+
+
+// btn btn-success / btn btn-danger ! income messages 0 - nixuya netu // search bar golovna storinka (rename searchbarpokemons), from last commit restore searchbarpokemons /
